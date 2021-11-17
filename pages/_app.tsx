7a356921dc2c4d12dfd1f2b7e64fd2fe73b0aa22
RegistryDev <registry.develop@gmail.com>
2021-11-18 00:20:25 +0900

    fix: apply title & Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import { AppProps } from 'next/app';
+import Header from '../components/Header';
 import GlobalStyle from '../styles/GlobalStyle';
 
 const app = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <title>Registry's TodoList</title>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
     </>
   );
